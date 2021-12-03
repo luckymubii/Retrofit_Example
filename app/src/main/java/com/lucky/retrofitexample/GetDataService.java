6815eda655ec12cd6c10d3a41b81aaf031375c2d
HP <bse163056@cust.pk>
2021-12-03 12:31:32 +0500

    Retrofit Example

---
+++ app/src/main/java/com/lucky/retrofitexample/GetDataService.java
@@ -0,0 +1,13 @@
+package com.lucky.retrofitexample;
+
+import com.lucky.retrofitexample.pojo.RetroPhoto;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+    @GET("/photos")
+    Call<List<RetroPhoto>> getAllPhotos();
+}
